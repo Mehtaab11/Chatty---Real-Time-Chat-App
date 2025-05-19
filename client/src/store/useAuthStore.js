95ffe3e39c9ffa49c0aa97884ce82b4b26306344
Mehtaab <mehtaabaalam117@gmail.com>
2025-05-19 08:24:41 +0530

    Frontend Preparation

---
+++ client/src/store/useAuthStore.js
@@ -0,0 +1,10 @@
+import { create } from "zustand"
+
+export const useAuthStore = create((set) => ({
+    authUser: null,
+    isSigningUp : false,
+    isLoggingIng : false,
+    isCheckingAuth: true,
+
+
+}))
