aab37f90b1b257676f091819692ca61f1025794c
Mehtaab <mehtaabaalam117@gmail.com>
2025-05-24 18:28:09 +0530

    Add Cursor-pointer at a few places

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -34,7 +34,7 @@ const Sidebar = () => {
             key={user._id}
             onClick={() => setSelectedUser(user)}
             className={`
-              w-full p-3 flex items-center gap-3
+              w-full p-3 cursor-pointer flex items-center gap-3
               hover:bg-base-300 transition-colors
               ${
                 selectedUser?._id === user._id
