54fd781bd7c98c3e56276e209653ee3d37e1201a
Mehtaab <mehtaabaalam117@gmail.com>
2025-05-28 17:03:07 +0530

    implementation of io emit

--- server/src/lib/socket.js
+++ server/src/lib/socket.js
@@ -18,8 +18,19 @@ io.on("connection", (socket) => {
 
     console.log(`User connected: ${socket.id}`);
 
+    const userId = socket.handshake.query.userId;
+
+    if (userId) userSocketMap[userId] = socket.id
+
+    // io emit is used to send request to all the connected clients
+    io.emit("getOnlineUsers", Object.keys(userSocketMap))
+
     socket.on("disconnect", () => {
         console.log(`User disconnected: ${socket.id}`);
+
+        delete userSocketMap(userId)
+        io.emit("getOnlineUsers", Object.keys(userSocketMap))
+
     });
 
     // You can add more event listeners here for different socket events
