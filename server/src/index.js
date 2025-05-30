efe7bc0b3049c3b95ad41c8c265a018a621b88fa
Mehtaab <mehtaabaalam117@gmail.com>
2025-05-30 18:32:58 +0530

    Production Setup

--- server/src/index.js
+++ server/src/index.js
@@ -1,16 +1,23 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser'
+import cors from "cors"
+
+import path from 'path';
+
+import { connectDB } from './lib/db.js';
+
 import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/mesage.route.js';
-import { connectDB } from './lib/db.js';
-import cors from "cors"
 import { app, server } from './lib/socket.js';
+
 
 dotenv.config();
 // const app = express();
 
 const PORT = 5001 || process.env.PORT;
+const __dirname = path.resolve();
+
 
 // app.use(express.json());
 app.use(express.json({ limit: '5mb' })); // or even '10mb' if needed
@@ -25,6 +32,15 @@ app.use(cors({
 app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
 
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../client/dist")));
+
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../client", "dist", "index.html"));
+    });
+
+}
 
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
